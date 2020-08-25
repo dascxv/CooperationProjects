7f80f8071782a05bf4b3f1257c36425fb491629c
1582991483 <1582991483@qq.com>
2020-08-25 15:48:22 +0800

    小项目

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,38 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/chenshuaibo',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/chenshuaibo.vue')
+  },
+  {
+    path: '/zhengxinle',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/zhengxinle.vue')
+  },
+  {
+    path: '/gongkexin',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/gongkexin.vue')
+  },
+  {
+    path: '/zhangpanpan',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/zhangpanpan.vue')
   }
 ]
 
